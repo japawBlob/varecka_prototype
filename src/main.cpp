559fcdd7063f336ae759b83a2df16701bfb6dc71
Jakub Jíra <jirajak2@fel.cvut.cz>
2022-12-19 22:11:49 +0100

    Minor bugfixing, beeping working.

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 
 #define LED1 26
 #define BUZZ 19
-#define ADD_BUTTON 27
-#define SUB_BUTTON 28
+#define ADD_BUTTON 34
+#define SUB_BUTTON 35
 
 void setup() {
     pinMode(LED1, OUTPUT);
@@ -21,10 +21,11 @@ unsigned long loud_time = 500;
 bool SET = false;
 unsigned add_pressed = 0;
 unsigned sub_pressed = 0;
+bool pressed = false;
 const unsigned debounce_time = 10;
 
 void buzz_buzz () {
-    analogWrite(BUZZ, 200);
+    analogWrite(BUZZ, 100);
     delay(1000);
     analogWrite(BUZZ, 0);
 }
@@ -45,8 +46,10 @@ void loop() {
         Serial.println(SET);
         if (SET) {
             digitalWrite(LED1, LOW);
+            analogWrite(BUZZ, 0);
         } else {
             digitalWrite(LED1, HIGH);
+            analogWrite(BUZZ, 10);
         }
         SET = !SET;
         last_now = now;
@@ -54,31 +57,44 @@ void loop() {
     if ( digitalRead(ADD_BUTTON) == LOW && digitalRead(SUB_BUTTON) == LOW) {
         add_pressed = 0;
         sub_pressed = 0;
+        Serial.println("Both pressed");
     } else {
 
         /// Handle ADD button
         if ( digitalRead(ADD_BUTTON) == LOW ) {
             add_pressed++;
+
         } else {
             add_pressed = 0;
         }
-        if ( digitalRead(ADD_BUTTON) == LOW && add_pressed > 5 ) {
+        if ( digitalRead(ADD_BUTTON) == LOW && add_pressed > 5 && !pressed ) {
             delay(debounce_time);
             if ( digitalRead(ADD_BUTTON) == LOW ) {
                 silence_time += 500;
+                pressed = true;
+                Serial.print("Add - new value: ");
+                Serial.println(silence_time);
             }
         }
         /// Handle SUB button
         if ( digitalRead(SUB_BUTTON) == LOW ) {
             sub_pressed++;
+
         } else {
             sub_pressed = 0;
         }
-        if ( digitalRead(ADD_BUTTON) == LOW && sub_pressed > 5 ) {
+        if ( digitalRead(SUB_BUTTON) == LOW && sub_pressed > 5 && !pressed ) {
             delay(debounce_time);
-            if ( digitalRead(ADD_BUTTON) == LOW ) {
+            if ( digitalRead(SUB_BUTTON) == LOW ) {
                 silence_time -= 500;
+                pressed = true;
+                Serial.print("Sub - new value: ");
+                Serial.println(silence_time);
             }
         }
-    }    
+    }
+    if ( pressed && digitalRead(ADD_BUTTON) == HIGH && digitalRead(SUB_BUTTON) == HIGH) {
+        pressed = false;
+        Serial.println("Reset");
+    }
 }
